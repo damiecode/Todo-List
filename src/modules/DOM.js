97b2b0906a21b9e8d8d7683648e07db6ad0af335
damiecode <damidigi@yahoo.com>
2020-02-11 22:17:05 +0100

    Refactor DOM

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,161 +1,110 @@
+
 import * as LocalStorage from './localstorage';
 
 const projectList = document.getElementById('project-list');
 const main = document.getElementById('main');
 const menu = document.getElementById('menu');
+const form = `<div class="form-group text-left">
+                <label for="todo-title">Task:</label>
+                <input type="text" class="form-control" id="todo-title" placeholder="Task">
+              </div>
+              <div class="form-group text-left">
+                <label for="todo-description">Description:</label>
+                <textarea class="form-control" id="todo-description" rows="2"></textarea>
+              </div>
+              <div class="form-group text-left">
+                <label for="todo-priority">Priority:</label>
+                <select class="form-control" id="todo-priority">
+                  <option>High</option>
+                  <option>Medium</option>
+                  <option>Low</option>
+                </select>
+              </div>
+              <div class="form-group text-left">
+                <label for="due-date">Due date:</label>
+                <input class="form-control" type="date" id="due-date">
+              </div>
+              <div id='form-buttons' class="mb-3">
+                <button type="button" id="create-todo" class="btn btn-sm btn-primary">Create Todo</button>
+                <button type="button" id="edit-todo" class="d-none">Edit Todo</button>
+                <button type="button" id="cancel" class="btn btn-sm btn-danger">Cancel</button>
+              </div>`;
 
 const todo = (project) => {
+  const addTodo = document.createElement('button');
   const container = document.createElement('form');
-  container.classList.add('todoForm');
-  container.id = 'todoForm';
-  container.style.display = 'none';
+  container.id = 'todo-form';
+  container.classList = 'd-none';
+  addTodo.id = 'add-todo-btn';
+  addTodo.innerHTML = 'Add Todo';
+  addTodo.classList = 'btn btn-primary btn-block mt-3 mb-3';
+  main.appendChild(container);
+  main.appendChild(addTodo);
 
-  const button = document.createElement('button');
-  button.classList.add('todoButton', 'btn', 'btn-primary', 'text-center');
-  button.id = 'todoButton'
-  button.innerText = 'Add Todo';
-  button.addEventListener('click', () => {
-    const container = document.getElementById('todoForm');
-    container.style.display = 'block';
-    const button = document.getElementById('todoButton');
-    button.style.display = 'none';
+  container.innerHTML = form;
+
+  const todoForm = document.getElementById('todo-form');
+  addTodo.addEventListener('click', () => {
+    todoForm.classList = '';
+    addTodo.classList = 'd-none';
   });
-
-  const title = document.createElement('input');
-  title.classList.add('form-control');
-  title.id = 'todo-title';
-  title.name = 'title';
-  title.setAttribute('type', 'text');
-
-  const titlelabel = document.createElement('label');
-  titlelabel.setAttribute('for', 'todo-title');
-  titlelabel.innerText = 'Name';
-
-  const description = document.createElement('textarea');
-  description.classList.add('form-control');
-  description.id = 'todo-description';
-  description.name = 'description';
-
-  const descriptionlabel = document.createElement('label');
-  descriptionlabel.setAttribute('for', 'todo-description');
-  descriptionlabel.innerText = 'Description';
-
-  const dueDate = document.createElement('input');
-  dueDate.classList.add('form-control', 'bg-primary');
-  dueDate.id = 'due-date';
-  dueDate.name = 'date';
-  dueDate.setAttribute('type', 'date');
-
-  const datelabel = document.createElement('label');
-  datelabel.setAttribute('for', 'due-date');
-  datelabel.innerText = 'Date';
-
-  const priority = document.createElement('select');
-  priority.classList.add('form-control');
-  priority.id = 'todo-priority';
-  priority.name = 'priority';
-
-  const option1 = document.createElement('option');
-  option1.value = 'High';
-  option1.innerText = 'High';
-
-  const option2 = document.createElement('option');
-  option2.value = 'Medium';
-  option2.innerText = 'Medium';
-
-  const option3 = document.createElement('option');
-  option3.value = 'Low';
-  option3.innerText = 'Low';
-
-  priority.append(option1, option2, option3);
-
-  const prioritylabel = document.createElement('label');
-  prioritylabel.setAttribute('for', 'todo-priority');
-  prioritylabel.innerText = 'Priority';
 
   const todoTitle = document.getElementById('todo-title');
   const todoDescription = document.getElementById('todo-description');
   const todoDueDate = document.getElementById('due-date');
   const todoPriority = document.getElementById('todo-priority');
 
-  const submit = document.createElement('button');
-  submit.innerText = 'Submit';
-  submit.classList.add('btn', 'btn-success');
-  submit.id = 'create-todo';
-  submit.addEventListener('click', () => {
-    LocalStorage.createTodo(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value, project)
+  const createTodoButton = document.getElementById('create-todo');
+  createTodoButton.addEventListener('click', () => {
+    LocalStorage.createTodo(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value, project);
     project(LocalStorage.getObject(project.title));
   });
 
-  const edit = document.createElement('button');
-  edit.innerText = 'Edit';
-  edit.classList.add('btn', 'btn-primary');
-  edit.id = 'edit-task';
-
-  const cancel = document.createElement('button');
-  cancel.classList.add('btn', 'btn-danger');
-  cancel.innerText = 'Cancel';
-  cancel.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    const container = document.querySelector('.todoForm');
+  const cancelButton = document.getElementById('cancel');
+  cancelButton.addEventListener('click', () => {
     container.classList = 'd-none';
-    cancel.classList = 'btn btn-sm btn-primary';
-    document.getElementById('edit-task').classList = 'd-none';
-    const button = document.querySelector('.todotButton');
-    button.classList = 'btn btn-primary btn-block';
+    createTodoButton.classList = 'btn btn-sm btn-primary';
+    document.getElementById('edit-todo').classList = 'd-none';
+    addTodo.classList = 'btn btn-primary btn-block';
   });
-
-  container.append(titlelabel,
-    title,
-    descriptionlabel,
-    description, datelabel,
-    dueDate,
-    prioritylabel,
-    priority,
-    submit,
-    cancel);
-  main.appendChild(container);
-  main.appendChild(button);
-  return main;
 };
 
 const showTodoList = (project) => {
   const presentProject = LocalStorage.getObject(project.title);
-  const listArr = presentProject.todoList;
+  const todoArr = presentProject.todoList;
   const listDiv = document.createElement('div');
-  listDiv.classList.add('row', 'd-flex', 'justify-content-center');
+  listDiv.classList = 'row d-flex justify-content-center';
 
   main.appendChild(listDiv);
 
-  for (let i = 0; i < listArr.length; i++) {
+  for (let i = 0; i < todoArr.length; i++) {
     const card = document.createElement('div');
-    card.classList = 'card', 'col-6';
+    card.classList = 'card col-6';
     card.style = 'background-color:#F5F5F5';
     card.innerHTML = `
-      <div class="card-body">
-      <div class="row">
-        <h5 class="card-title col-10">${listArr[i].title}</h5>
-        <button type="button" id="edit-todo-${i}" class="btn btn-sm btn-secondary col-2">Edit</button>
-      </div>
-        <p class="card-text">${listArr[i].description}</p>
-        <span class='text-muted'>${listArr[i].dueDate}</span>
-        <span class='text-warning float-right'> ${listArr[i].priority}</span>
-        <button class="btn btn-success btn-block mt-5 tick js-tick" id="complete-task-${i}">Complete Task</button>
-      </div>`;
+                      <div class="card-body" >
+											<div class="row">
+	                      <h5 class="card-title col-10">${todoArr[i].title}</h5>
+												<button type="button" id="edit-todo-btn-${i}" class="btn btn-sm btn-secondary col-2">Edit</button>
+											</div>
+                        <p class="card-text">${todoArr[i].description}</p>
+                        <span class='text-muted'>${todoArr[i].dueDate}</span>
+                        <span class='text-warning float-right'> ${todoArr[i].priority}</span>
+                        <button class="btn btn-success btn-block mt-5" id="complete-task-${i}">Complete Task</button>
+                      </div>`;
     listDiv.appendChild(card);
-    console.log(card);
-    const editButton = document.getElementById(`edit-todo-${i}`);
+    const editButton = document.getElementById(`edit-todo-btn-${i}`);
     editButton.addEventListener('click', () => {
-      document.getElementById('todoButton').classList = 'd-none';
-      document.getElementById('create-task').classList = 'd-none';
-      document.getElementById('todoForm').classList = '';
-      document.getElementById('edit-task').classList = 'btn btn-primary';
-      document.getElementById('todo-title').value = listArr[i].title;
-      document.getElementById('todo-description').value = listArr[i].description;
-      document.getElementById('due-date').value = listArr[i].dueDate;
-      document.getElementById('todo-priority').value = listArr[i].priority;
+      document.getElementById('add-todo-btn').classList = 'd-none';
+      document.getElementById('create-todo').classList = 'd-none';
+      document.getElementById('todo-form').classList = '';
+      document.getElementById('edit-todo').classList = 'btn btn-primary';
+      document.getElementById('todo-title').value = todoArr[i].title;
+      document.getElementById('todo-description').value = todoArr[i].description;
+      document.getElementById('due-date').value = todoArr[i].dueDate;
+      document.getElementById('todo-priority').value = todoArr[i].priority;
 
-      document.getElementById('edit-task').addEventListener('click', () => {
+      document.getElementById('edit-todo').addEventListener('click', () => {
         presentProject.todoList[i].title = document.getElementById('todo-title').value;
         presentProject.todoList[i].description = document.getElementById('todo-description').value;
         presentProject.todoList[i].dueDate = document.getElementById('due-date').value;
@@ -165,47 +114,47 @@ const showTodoList = (project) => {
       });
     });
 
-    const complete = document.getElementById('complete-task-${i}');
-    complete.addEventListener('click', () => {
+    const completeButton = document.getElementById(`complete-task-${i}`);
+    completeButton.addEventListener('click', () => {
       presentProject.todoList.splice(i, 1);
       LocalStorage.setObject(presentProject, presentProject.title);
-      Project(LocalStorage.getObject(presentProject.title));
+      project(LocalStorage.getObject(presentProject.title));
     });
-  };
-}
+  }
+};
 
 const project = (project) => {
   if (project) {
     while (main.firstChild) {
       main.removeChild(main.firstChild);
-    };
+    }
+
     const projectTitleDiv = document.createElement('div');
 
-    projectTitleDiv.innerHTML = `<h3 class='display-4 mt-4 mb-3 bg-white text-center rounded'>${project.title}</h3>`;
+    projectTitleDiv.innerHTML = `<h3 class='mt-4 mb-3 bg-white text-center rounded'>${project.title}</h3>`;
     main.appendChild(projectTitleDiv);
 
     todo(project);
     showTodoList(project);
-  };
+  }
 };
 
 const renderPage = () => {
   const projectsArr = LocalStorage.showProjects();
-  const projectsSection = document.createElement('div');
-  projectsSection.classList.add('m-4');
-  projectsSection.innerHTML = `
-  <input type="text" id="project-title" required>
-  <button class="btn btn-sm btn-primary" id="add-project">Add a Project</button>
-  `
-  menu.appendChild(projectsSection);
 
-  const projectBtn = document.getElementById('add-project');
-  projectBtn.addEventListener('click', () => {
+  const projectSection = document.createElement('div');
+  projectSection.classList = 'm-4';
+  projectSection.innerHTML = `<input type="text" id="project-title" required>
+                            <button class="btn btn-sm btn-primary" id="add-project">Add Project</button>`;
+  menu.appendChild(projectSection);
+
+  const addProjectBtn = document.getElementById('add-project');
+  addProjectBtn.addEventListener('click', () => {
     const projectTitle = document.getElementById('project-title');
     if (projectTitle.value.length > 0) {
       LocalStorage.createProject(projectTitle.value);
       projectList.innerHTML = '';
-      projectsSection.innerHTML = '';
+      projectSection.innerHTML = '';
       renderPage();
       project(LocalStorage.getObject(projectTitle.value));
     } else {
@@ -216,9 +165,8 @@ const renderPage = () => {
   for (let i = 0; i < projectsArr.length; i++) {
     const projectPageItem = document.createElement('li');
     projectPageItem.classList.add('list-group-item');
-    projectPageItem.innerHTML = `
-      <button class='btn btn-link text-center'>${projectsArr[i].title}</button>
-      <button id='remove-${i}' class="btn btn-danger btn-sm float-right">delete</button>`
+    projectPageItem.innerHTML = `<button class="btn btn-link">${projectsArr[i].title}</button>
+    <button id='remove-${i}' class='btn btn-danger btn-sm float-right'>X</button>`;
     projectPageItem.firstChild.addEventListener('click', () => {
       project(projectsArr[i]);
     });
@@ -227,11 +175,16 @@ const renderPage = () => {
     removeBtn.addEventListener('click', () => {
       LocalStorage.deleteObject(removeBtn.parentElement.firstChild.innerHTML);
       projectList.innerHTML = '';
-      projectsSection.innerHTML = '';
+      projectSection.innerHTML = '';
       main.innerHTML = '';
       renderPage();
     });
   }
 };
 
-export { renderPage, project, todo, showTodoList, };
+export {
+  renderPage,
+  project,
+  todo,
+  showTodoList,
+};
