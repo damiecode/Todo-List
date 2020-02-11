2022b4fb639bf09c2680642463e612ce103c3fd9
damiecode <damidigi@yahoo.com>
2020-02-11 19:13:09 +0100

    Improve project feature

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -25,39 +25,35 @@ const todo = (project) => {
   title.classList.add('form-control');
   title.id = 'todo-title';
   title.name = 'title';
-  title.value = '';
-  title.type = 'text';
+  title.setAttribute('type', 'text');
 
   const titlelabel = document.createElement('label');
-  titlelabel.for = 'title';
+  titlelabel.setAttribute('for', 'todo-title');
   titlelabel.innerText = 'Name';
 
   const description = document.createElement('textarea');
   description.classList.add('form-control');
   description.id = 'todo-description';
   description.name = 'description';
-  description.value = '';
 
   const descriptionlabel = document.createElement('label');
-  descriptionlabel.for = 'description';
+  descriptionlabel.setAttribute('for', 'todo-description');
   descriptionlabel.innerText = 'Description';
 
   const dueDate = document.createElement('input');
   dueDate.classList.add('form-control', 'bg-primary');
   dueDate.id = 'due-date';
   dueDate.name = 'date';
-  dueDate.value = '';
-  dueDate.type = 'date';
+  dueDate.setAttribute('type', 'date');
 
   const datelabel = document.createElement('label');
-  datelabel.for = 'date';
+  datelabel.setAttribute('for', 'due-date');
   datelabel.innerText = 'Date';
 
   const priority = document.createElement('select');
   priority.classList.add('form-control');
   priority.id = 'todo-priority';
   priority.name = 'priority';
-  priority.value = '';
 
   const option1 = document.createElement('option');
   option1.value = 'High';
@@ -74,7 +70,7 @@ const todo = (project) => {
   priority.append(option1, option2, option3);
 
   const prioritylabel = document.createElement('label');
-  prioritylabel.for = 'priority';
+  prioritylabel.setAttribute('for', 'todo-priority');
   prioritylabel.innerText = 'Priority';
 
   const todoTitle = document.getElementById('todo-title');
@@ -85,6 +81,7 @@ const todo = (project) => {
   const submit = document.createElement('button');
   submit.innerText = 'Submit';
   submit.classList.add('btn', 'btn-success');
+  submit.id = 'create-todo';
   submit.addEventListener('click', () => {
     LocalStorage.createTodo(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value, project)
     project(LocalStorage.getObject(project.title));
@@ -98,10 +95,11 @@ const todo = (project) => {
   const cancel = document.createElement('button');
   cancel.classList.add('btn', 'btn-danger');
   cancel.innerText = 'Cancel';
-  cancel.addEventListener('click', () => {
+  cancel.addEventListener('click', (ev) => {
+    ev.preventDefault();
     const container = document.querySelector('.todoForm');
     container.classList = 'd-none';
-    submit.classList = 'btn btn-sm btn-primary';
+    cancel.classList = 'btn btn-sm btn-primary';
     document.getElementById('edit-task').classList = 'd-none';
     const button = document.querySelector('.todotButton');
     button.classList = 'btn btn-primary btn-block';
@@ -118,6 +116,7 @@ const todo = (project) => {
     cancel);
   main.appendChild(container);
   main.appendChild(button);
+  return main;
 };
 
 const showTodoList = (project) => {
@@ -133,7 +132,7 @@ const showTodoList = (project) => {
     card.classList = 'card', 'col-6';
     card.style = 'background-color:#F5F5F5';
     card.innerHTML = `
-      <div class="card-body" >
+      <div class="card-body">
       <div class="row">
         <h5 class="card-title col-10">${listArr[i].title}</h5>
         <button type="button" id="edit-todo-${i}" class="btn btn-sm btn-secondary col-2">Edit</button>
@@ -141,14 +140,15 @@ const showTodoList = (project) => {
         <p class="card-text">${listArr[i].description}</p>
         <span class='text-muted'>${listArr[i].dueDate}</span>
         <span class='text-warning float-right'> ${listArr[i].priority}</span>
-        <button class="btn btn-success btn-block mt-5" id="complete-task-${i}">Complete Task</button>
+        <button class="btn btn-success btn-block mt-5 tick js-tick" id="complete-task-${i}">Complete Task</button>
       </div>`;
     listDiv.appendChild(card);
+    console.log(card);
     const editButton = document.getElementById(`edit-todo-${i}`);
     editButton.addEventListener('click', () => {
-      document.getElementById('add-task-btn').classList = 'd-none';
+      document.getElementById('todoButton').classList = 'd-none';
       document.getElementById('create-task').classList = 'd-none';
-      document.getElementById('todo-form').classList = '';
+      document.getElementById('todoForm').classList = '';
       document.getElementById('edit-task').classList = 'btn btn-primary';
       document.getElementById('todo-title').value = listArr[i].title;
       document.getElementById('todo-description').value = listArr[i].description;
@@ -217,12 +217,8 @@ const renderPage = () => {
     const projectPageItem = document.createElement('li');
     projectPageItem.classList.add('list-group-item');
     projectPageItem.innerHTML = `
-    <li class="todo-item">
-      <input id='completed' type="checkbox">
-      <label for="completed" class="tick js-tick"></label>
-      <button class='btn-link'>${projectsArr[i].title}</button>
-      <button id='remove-${i}' class="delete-todo js-delete-todo btn btn-danger btn-sm float-right">X</button>
-    </li>`
+      <button class='btn btn-link text-center'>${projectsArr[i].title}</button>
+      <button id='remove-${i}' class="btn btn-danger btn-sm float-right">delete</button>`
     projectPageItem.firstChild.addEventListener('click', () => {
       project(projectsArr[i]);
     });
@@ -238,4 +234,4 @@ const renderPage = () => {
   }
 };
 
-export { renderPage, project, todo, showTodoList };
+export { renderPage, project, todo, showTodoList, };
